3f613b00f22f6181a0c150d7a733bf505ef0764d
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-22 22:13:22 +0200

    Load WebComm UI overrides

--- app/entry.py
+++ app/entry.py
@@ -3,5 +3,6 @@ from . import direct_webcomm  # noqa: F401
 from . import direct_webcomm_compat  # noqa: F401
 from . import webcomm_compat  # noqa: F401
 from . import webcomm_management  # noqa: F401
+from . import webcomm_ui_overrides  # noqa: F401
 
 app = main.app
