455b435314d08cfbf31a1780594a34a133d05797
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-22 22:02:28 +0200

    Enable WebComm scheduler and reset management

--- app/entry.py
+++ app/entry.py
@@ -2,5 +2,6 @@ from . import main
 from . import direct_webcomm  # noqa: F401
 from . import direct_webcomm_compat  # noqa: F401
 from . import webcomm_compat  # noqa: F401
+from . import webcomm_management  # noqa: F401
 
 app = main.app
