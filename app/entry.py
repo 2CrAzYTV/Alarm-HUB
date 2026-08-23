98a806e6522bc78164e3c8e12e7d3aa75c13c98e
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-23 12:31:12 +0200

    Register start location mapping API

--- app/entry.py
+++ app/entry.py
@@ -11,5 +11,6 @@ from . import guides_dropdown  # noqa: F401
 from . import nav_dashboard  # noqa: F401
 from . import integrations_ui  # noqa: F401
 from . import travel_time  # noqa: F401
+from . import start_locations_api  # noqa: F401
 
 app = main.app
