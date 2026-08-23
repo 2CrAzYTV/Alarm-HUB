0ec8f6c1548e257d665de62274592f03e9e82bb6
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-23 10:58:25 +0200

    Fix Dashboard nav detection on alarms page

--- app/nav_dashboard.py
+++ app/nav_dashboard.py
@@ -8,13 +8,24 @@ _original_layout = main._layout
 
 def _layout_with_dashboard(title: str, body: str, user: main.User | None = None) -> str:
     html = _original_layout(title, body, user)
-    if user and "href='/'>Dashboard</a>" not in html:
-        html = html.replace(
-            "<a href='/alarms'>Meine Wecker</a>",
-            "<a href='/'>Dashboard</a><a href='/alarms'>Meine Wecker</a>",
-            1,
-        )
-    return html
+    if not user:
+        return html
+
+    nav_start = html.find("<nav>")
+    nav_end = html.find("</nav>", nav_start)
+    if nav_start == -1 or nav_end == -1:
+        return html
+
+    nav_html = html[nav_start:nav_end]
+    if "href='/'>Dashboard</a>" in nav_html:
+        return html
+
+    nav_html = nav_html.replace(
+        "<a href='/alarms'>Meine Wecker</a>",
+        "<a href='/'>Dashboard</a><a href='/alarms'>Meine Wecker</a>",
+        1,
+    )
+    return html[:nav_start] + nav_html + html[nav_end:]
 
 
 main._layout = _layout_with_dashboard
