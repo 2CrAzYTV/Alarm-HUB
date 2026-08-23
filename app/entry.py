dd86bedf3b4eae80440c3b78e6c1adc9de2f1d1f
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-23 09:55:21 +0200

    Load beginner smartphone setup guides

--- app/entry.py
+++ app/entry.py
@@ -1,4 +1,5 @@
 from . import main
+from . import guides  # noqa: F401
 from . import direct_webcomm  # noqa: F401
 from . import direct_webcomm_compat  # noqa: F401
 from . import webcomm_compat  # noqa: F401
