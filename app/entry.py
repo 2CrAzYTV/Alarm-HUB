4f1d6ba3b5be404b22608bf143a46fd9a9d5bad4
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-23 13:09:30 +0200

    Register service card API

--- app/entry.py
+++ app/entry.py
@@ -13,5 +13,6 @@ from . import integrations_ui  # noqa: F401
 from . import travel_time  # noqa: F401
 from . import start_locations_api  # noqa: F401
 from . import calendar_files  # noqa: F401
+from . import service_cards  # noqa: F401
 
 app = main.app
